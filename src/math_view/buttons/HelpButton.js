3a2c304c9bbb1a47903c2f8d02541af751455295
bl96 <47901930+bl96@users.noreply.github.com>
2020-05-06 12:43:37 +0200

    V5.1.3 CHANGED_HELP_BUTTON_ICON
    - now using fontawesome icon instead of "?"

--- src/math_view/buttons/HelpButton.js
+++ src/math_view/buttons/HelpButton.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Button} from "react-bootstrap";
+import FontAwesome from "react-fontawesome";
 
 const HelpButton = ({dataTarget}) => (
-    <Button style={{padding:"0.0rem 0.5rem"}} variant={'secondary'} size={"sm"} title='Pomoc' data-toggle='collapse' data-target={dataTarget} aria-expanded='false' aria-controls='collapseExample'>?</Button>
+    <Button style={{padding:"0.2rem 0.4rem"}} variant={'secondary'} size={"sm"} title='Pomoc' data-toggle='collapse' data-target={dataTarget} aria-expanded='false' aria-controls='collapseExample'>
+        <FontAwesome name='fas fa-question'/>
+    </Button>
 );
 
 export default HelpButton;

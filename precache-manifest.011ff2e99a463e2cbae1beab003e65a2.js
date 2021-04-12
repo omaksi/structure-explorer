46438fabfcdb66fece78bae9e4c0573db065e5c0
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2021-04-12 21:11:26 +0200

    rebuilt with graph and Henkin-Hintikka game

---
+++ precache-manifest.011ff2e99a463e2cbae1beab003e65a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1839c05e022ba10349cdb7317f52307",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5d71c5264afa8ed871b",
+    "url": "./static/css/main.979758dc.chunk.css"
+  },
+  {
+    "revision": "4d6ac17d5726d169a8f1",
+    "url": "./static/js/2.d4630f24.chunk.js"
+  },
+  {
+    "revision": "809b730e1dfd2eddf4780b8f91e29ca1",
+    "url": "./static/js/2.d4630f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d71c5264afa8ed871b",
+    "url": "./static/js/main.0e0237e7.chunk.js"
+  },
+  {
+    "revision": "3f3fa262821a4a0bf139",
+    "url": "./static/js/runtime-main.6c50a1a6.js"
+  }
+]);

4c4516808d7618f9512f98a36bdf22030f8de4ad
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2022-02-22 12:23:45 +0100

    Simpler arrayEquals

--- src/redux/reducers/structure.js
+++ src/redux/reducers/structure.js
@@ -584,15 +584,10 @@ function  parsedToValue(parsedValues) {
 }
 
 function arraysEqual(array1, array2){
-  if(array1.length !== array2.length) {
-    return false;
-  }
-  for(let index = 0; index < array1.length; index++){
-    if(array1[index] !== array2[index]){
-      return false;
-    }
-  }
-  return true;
+  return (
+    (array1.length == array2.length) &&
+    array1.every( (elem, index) => elem === array2[index] )
+  );
 }
 
 export default structureReducer;
